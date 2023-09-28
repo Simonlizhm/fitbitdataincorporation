5c241bf1b1d6075aad8ddd8e694329221093c8b3
Simon Li <76879655+Simonlizhm@users.noreply.github.com>
2023-09-28 12:07:22 -0400

    bert_transformer.py

--- bert_transformer.py
+++ bert_transformer.py
@@ -6,21 +6,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 
-# Load the dataset
-data = pd.read_csv("medical_advice_dataset.csv")
 
-# Prepare the tokenizer
+data = pd.read_csv("medical_advice_dataset.csv")
 tokenizer = Tokenizer(num_words=5000, oov_token="<OOV>")
 tokenizer.fit_on_texts(data['text'])
 
-# Transform texts to sequences
 sequences = tokenizer.texts_to_sequences(data['text'])
 padded_sequences = pad_sequences(sequences, padding='post')
 
-# Split the dataset into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(padded_sequences, data['medical_advice'], test_size=0.2, random_state=1)
 
-# Define the model
 model = Sequential([
     Embedding(5000, 64, input_length=padded_sequences.shape[1]),
     LSTM(64, return_sequences=True),
@@ -30,11 +25,8 @@ model = Sequential([
     Dense(1, activation='sigmoid')
 ])
 
-# Define the optimizer
 optimizer = Adam(learning_rate=0.001)
 
-# Compile the model
 model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-# Train the model
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
